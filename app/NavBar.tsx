3c540ae61870168dbe04aace565b50e5c5185760
Nelsonlin0321 <nelsonlin0321@outlook.com>
2023-10-28 11:48:52 +0800

    Styling the active Link

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,7 +1,12 @@
+"use client";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import React from "react";
 import { AiFillBug } from "react-icons/ai";
+import classnames from "classnames";
+
 const NavBar = () => {
+  const currentPath = usePathname();
   const links = [
     { label: "Dashboard", href: "/" },
     { label: "Issues", href: "/issues" },
@@ -16,7 +21,14 @@ const NavBar = () => {
           <li key={link.href}>
             <Link
               href={link.href}
-              className="text-zinc-500 hover:text-zinc-800 transition-colors"
+              className={classnames({
+                "text-zinc-800": link.href == currentPath,
+                "text-zinc-500": link.href != currentPath,
+                "hover:text-zinc-800 transition-color": true,
+              })}
+              //         {`${
+              //     link.href == currentPath ? "text-zinc-800" : "text-zinc-500"
+              //   } hover:text-zinc-800 transition-colors"`}
             >
               {link.label}
             </Link>
---
+++ app/issues/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const IssuePage = () => {
+  return <div>IssuePage</div>;
+};
+
+export default IssuePage;
