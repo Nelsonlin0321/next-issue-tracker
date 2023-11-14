2eacfa15fc01574333c62c143d73012c5804c56a
Nelsonlin0321 <nelsonlin0321@outlook.com>
2023-11-14 22:25:48 +0800

    Adding confirmation Dialog

--- app/issues/[id]/DeleteIssueButton.tsx
+++ app/issues/[id]/DeleteIssueButton.tsx
@@ -1,8 +1,34 @@
-import { Button } from "@radix-ui/themes";
+"use client";
+import { Button, Flex } from "@radix-ui/themes";
 import React from "react";
+import { AlertDialog } from "@radix-ui/themes";
 
 const DeleteIssueButton = ({ issueId }: { issueId: number }) => {
-  return <Button color="red">Delete Issue</Button>;
+  return (
+    <AlertDialog.Root>
+      <AlertDialog.Trigger>
+        <Button color="red">Delete Issue</Button>
+      </AlertDialog.Trigger>
+      <AlertDialog.Content>
+        <AlertDialog.Title>Confirm Deletion</AlertDialog.Title>
+        <AlertDialog.Description>
+          Are you sure to delete this issue. The action cannot be undone.
+        </AlertDialog.Description>
+        <Flex gap="3" mt="4" justify="end">
+          <AlertDialog.Cancel>
+            <Button variant="soft" color="gray">
+              Cancel
+            </Button>
+          </AlertDialog.Cancel>
+          <AlertDialog.Action>
+            <Button variant="solid" color="red">
+              Delete
+            </Button>
+          </AlertDialog.Action>
+        </Flex>
+      </AlertDialog.Content>
+    </AlertDialog.Root>
+  );
 };
 
 export default DeleteIssueButton;
