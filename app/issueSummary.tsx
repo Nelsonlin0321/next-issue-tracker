c0cc01b7fc0012084505b68d3da98afc1c8ea0c4
Nelsonlin0321 <nelsonlin0321@outlook.com>
2023-11-23 23:56:02 +0800

    Building summary issue

---
+++ app/issueSummary.tsx
@@ -0,0 +1,39 @@
+import { Status } from "@prisma/client";
+import { Card, Flex, Link, Text } from "@radix-ui/themes";
+import React from "react";
+
+interface Props {
+  open: number;
+  inProgress: number;
+  closed: number;
+}
+
+const IssueSummary = ({ open, inProgress, closed }: Props) => {
+  const containers: { label: string; value: number; status: Status }[] = [
+    { label: "Open Issues", value: open, status: "OPEN" },
+    { label: "In-Progress Issues", value: inProgress, status: "IN_PROGRESS" },
+    { label: "Closed Issues", value: closed, status: "CLOSED" },
+  ];
+
+  return (
+    <Flex gap="4">
+      {containers.map((container) => (
+        <Card key={container.label}>
+          <Flex direction="column">
+            <Link
+              href={`/issues?status=${container.status}`}
+              className="text-sm font-medium"
+            >
+              {container.label}
+            </Link>
+            <Text size="5" className="font-bold">
+              {container.value}
+            </Text>
+          </Flex>
+        </Card>
+      ))}
+    </Flex>
+  );
+};
+
+export default IssueSummary;
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,35 @@
+import prisma from "@/prisma/client";
 import LatestIssue from "./LatestIssue";
+import IssueSummary from "./issueSummary";
+import { isAsync } from "zod";
+import { Status } from "@prisma/client";
 
 interface Props {
   searchParams: { page: string };
 }
 
-export default function Home({ searchParams }: Props) {
-  return <LatestIssue />;
-}
+const Home = async ({ searchParams }: Props) => {
+  // return <LatestIssue />;
+  const statusCountList = await prisma.issue.groupBy({
+    by: "status",
+    _count: {
+      id: true,
+    },
+  });
+
+  const getStatusCount = (status: Status) => {
+    const item = statusCountList.find((item) => item.status == status);
+
+    return item ? item._count.id : 0;
+  };
+
+  return (
+    <IssueSummary
+      open={getStatusCount("OPEN")}
+      inProgress={getStatusCount("IN_PROGRESS")}
+      closed={getStatusCount("CLOSED")}
+    />
+  );
+};
+
+export default Home;
