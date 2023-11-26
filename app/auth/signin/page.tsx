95feff0f0ae690232fe6dbf8ff37b678196dad86
Nelsonlin0321 <nelsonlin0321@outlook.com>
2023-11-26 12:25:16 +0800

    Finished user register

--- app/auth/signin/page.tsx
+++ app/auth/signin/page.tsx
@@ -3,67 +3,65 @@ import { Box, Button, Card, Flex, Text, TextField } from "@radix-ui/themes";
 import Image from "next/image";
 import googleIcon from "@/public/google.svg";
 import { signIn } from "next-auth/react";
+import Link from "next/link";
 
 const SigninPage = () => {
   return (
-    <>
-      <Card className=" p-5 mx-auto" style={{ maxWidth: 400 }}>
-        <Flex direction="column" gap="5">
-          <Box id="google">
-            <Button
-              className="w-full"
-              radius="full"
-              variant="soft"
-              color="gray"
-              size="4"
-              onClick={() => signIn("google", { callbackUrl: "/" })}
-            >
-              <Flex align="center" gap="5">
-                <Image
-                  src={googleIcon}
-                  alt="Google Icon"
-                  width="18"
-                  height="18"
-                />
-                <Text>Sign in with Google</Text>
-              </Flex>
-            </Button>
-          </Box>
-          <Flex className="text-center" align="center" gap="3">
-            <hr className="w-1/2" />
-            <Text className="whitespace-nowrap">Sign in with your email</Text>
-            <hr className="w-1/2" />
-          </Flex>
-          <Box id="credentials">
-            <Flex direction="column" gap="4">
-              <Flex direction="column" gap="2">
-                <label className="font-semibold">Email:</label>
-                <TextField.Input
-                  radius="large"
-                  placeholder="Enter your email"
-                />
-              </Flex>
-              <Flex direction="column" gap="2">
-                <label className="font-semibold">Password:</label>
-                <TextField.Input
-                  radius="large"
-                  placeholder="Enter your password"
-                  type="password"
-                />
-              </Flex>
-              <Flex gap="2" justify="end">
+    <Card className=" p-5 mx-auto" style={{ maxWidth: 400 }}>
+      <Flex direction="column" gap="5">
+        <Box id="google">
+          <Button
+            className="w-full"
+            radius="full"
+            variant="soft"
+            color="gray"
+            size="4"
+            onClick={() => signIn("google", { callbackUrl: "/" })}
+          >
+            <Flex align="center" gap="5">
+              <Image
+                src={googleIcon}
+                alt="Google Icon"
+                width="18"
+                height="18"
+              />
+              <Text>Sign in with Google</Text>
+            </Flex>
+          </Button>
+        </Box>
+        <Flex className="text-center" align="center" gap="3">
+          <hr className="w-1/2" />
+          <Text className="whitespace-nowrap">Sign in with your email</Text>
+          <hr className="w-1/2" />
+        </Flex>
+        <Box id="credentials">
+          <Flex direction="column" gap="4">
+            <Flex direction="column" gap="2">
+              <label className="font-semibold">Email:</label>
+              <TextField.Input radius="large" placeholder="Enter your email" />
+            </Flex>
+            <Flex direction="column" gap="2">
+              <label className="font-semibold">Password:</label>
+              <TextField.Input
+                radius="large"
+                placeholder="Enter your password"
+                type="password"
+              />
+            </Flex>
+            <Flex gap="2" justify="end">
+              <Link href="/auth/register">
                 <Button variant="soft">
                   <Text className=" font-bold">Create your account</Text>
                 </Button>
-                <Button>
-                  <Text className=" font-bold"> Sign In</Text>
-                </Button>
-              </Flex>
+              </Link>
+              <Button>
+                <Text className=" font-bold"> Sign In</Text>
+              </Button>
             </Flex>
-          </Box>
-        </Flex>
-      </Card>
-    </>
+          </Flex>
+        </Box>
+      </Flex>
+    </Card>
   );
 };
 
